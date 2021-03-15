6d1c70265127910e9f8072ae4085c29418066001
Ramon <ramon@ramonuribe.com>
2021-03-14 17:04:12 -0700

    initial commit

---
+++ src/test/java/com/ramonuribe/jenkinsExample/JenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonuribe.jenkinsExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
